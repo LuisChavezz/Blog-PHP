c6ef3fa91428d339469ff4ac2bf7bdd1cb6eeaef
LuisChavezz <ing.luischavezz@gmail.com>
2021-03-18 01:56:02 -0600

    Configuración del sitio y barra de la homepage

---
+++ admin/config.php
@@ -0,0 +1,20 @@
+<?php
+
+define('RUTA', 'http://localhost/cursophp/blog/');
+
+$bd_config = [
+    'bd' => 'blog_practica',
+    'user' => 'root',
+    'password' => ''
+];
+
+$blog_config = [
+    'post_per_page' => '3',
+    'folder_img' => 'img/'
+];
+
+/*$blog_admin = [
+
+];*/
+
+?>
