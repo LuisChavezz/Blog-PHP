18b78a3162df43ee40074b0df4ab9c919a47560d
LuisChavezz <ing.luischavezz@gmail.com>
2021-03-19 00:16:33 -0600

    Agregando section post

--- views/index.view.php
+++ views/index.view.php
@@ -31,5 +31,21 @@
             </div>
         </div>
     </header>
+
+    <div class="container">
+        <div class="post">
+            <article>
+                <h2 class="titulo">Titulo del artículo</h2>
+                <p class="fecha">Fecha del post</p>
+                <div class="thumb">
+                    <a href="#">
+                        <img src="<?php echo RUTA; ?>/img/1.jpg">
+                    </a>
+                </div>
+                <p class="extracto">Lorem ipsum dolor sit amet.</p>
+                <a href="#" class="continuar">Continuar leyendo...</a>
+            </article>
+        </div>
+    </div>
 </body>
 </html>
