c56aa6991537267749b0ac4a7d54a1d9055319ae
LuisChavezz <ing.luischavezz@gmail.com>
2021-03-23 00:25:42 -0600

    Rediseñado de los posts

--- views/index.view.php
+++ views/index.view.php
@@ -1,18 +1,48 @@
 <?php require 'views/header.php';?>
 
     <div class="container">
-        <div class="post">
-            <article>
-                <h1 class="titulo">Titulo del artículo</h1>
-                <p class="fecha">Fecha del post</p>
-                <div class="thumb">
-                    <a href="#">
-                        <img src="<?php echo RUTA; ?>/img/1.jpg">
-                    </a>
-                </div>
-                <p class="extracto">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-                <a href="#" class="continuar">Continuar leyendo...</a>
-            </article>
+        <div class="posts">
+            <div class="post">
+                <article>
+                    <h1 class="titulo">Titulo del artículo</h1>
+                    <p class="fecha">Fecha del post</p>
+                    <div class="thumb">
+                        <a href="#">
+                            <img src="<?php echo RUTA; ?>/img/1.jpg">
+                        </a>
+                    </div>
+                    <p class="extracto">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+                    <a href="#" class="continuar">Continuar leyendo...</a>
+                </article>
+            </div>
+
+            <div class="post">
+                <article>
+                    <h1 class="titulo">Titulo del artículo</h1>
+                    <p class="fecha">Fecha del post</p>
+                    <div class="thumb">
+                        <a href="#">
+                            <img src="<?php echo RUTA; ?>/img/1.jpg">
+                        </a>
+                    </div>
+                    <p class="extracto">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+                    <a href="#" class="continuar">Continuar leyendo...</a>
+                </article>
+            </div>
+
+            <div class="post">
+                <article>
+                    <h1 class="titulo">Titulo del artículo</h1>
+                    <p class="fecha">Fecha del post</p>
+                    <div class="thumb">
+                        <a href="#">
+                            <img src="<?php echo RUTA; ?>/img/1.jpg">
+                        </a>
+                    </div>
+                    <p class="extracto">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+                    <a href="#" class="continuar">Continuar leyendo...</a>
+                </article>
+            </div>
         </div>
 
         <div class="side-bar">
