988e54c890de8a084afabe4e240f78e18aa0bd9d
LuisChavezz <ing.luischavezz@gmail.com>
2021-03-25 23:52:12 -0600

    Get posts desde la base de datos

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
 function conexion ($bd_config) { 
     // Conexión a la base de datos
     try {
-        $conexion = new PDO('mysql:host=localhost;bdname=blog_practica', 'root', '');
+        $conexion = new PDO('mysql:host=localhost;dbname='.$bd_config['bd'], $bd_config['user'], $bd_config['password']);
         return $conexion;
 
     } catch (PDOException $e){
@@ -17,6 +17,24 @@ function cleanData ($data) {
     $data = htmlspecialchars(stripslashes(trim($data)));
     return $data;
 }
+
+// Obtener el número de página actual
+function current_page () {
+    return isset($_GET['p']) ? (int)$_GET['p'] : 1;
+}
+
+// Obtener posts
+function getPost ($post_per_page, $conexion) {
+    $inicio = ( current_page() > 1 ) ? current_page() * $post_per_page - $post_per_page : 0;
+
+    $statement = $conexion -> prepare("SELECT SQL_CALC_FOUND_ROWS * FROM articulos LIMIT $inicio, $post_per_page");
+    $statement -> execute();
+
+    return $statement -> fetchAll();
+
+}
+
+
 
 
 ?>
--- index.php
+++ index.php
@@ -2,5 +2,21 @@
 
 require 'admin/config.php';
 require 'functions.php';
+
+// Conexión al BD
+$conexion = conexion($bd_config); 
+
+if(!$conexion) { //Sí existe la conexion a la BD
+    header('Location: error.php');
+}
+
+// Get de los posts
+$posts = getPost ($blog_config['post_per_page'], $conexion);
+
+if(!$posts) { //Sí no hay posts
+    header('Location: error.php');
+}
+
+//
 require 'views/index.view.php';
 ?>
