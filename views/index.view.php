714dbe31061ab8bc1dfc3565eb34c9a785c63a7b
LuisChavezz <ing.luischavezz@gmail.com>
2021-03-28 20:01:53 -0600

    Cargando los posts a la vista

--- views/index.view.php
+++ views/index.view.php
@@ -2,48 +2,25 @@
 
     <div class="container">
         <div class="posts">
-            <div class="post">
-                <article>
-                    <h1 class="titulo"><a href="#">Titulo del artículo</a></h1>
-                    <p class="fecha">Fecha del post</p>
-                    <div class="thumb">
-                        <a href="#">
-                            <img src="<?php echo RUTA; ?>/img/1.jpg">
-                        </a>
-                    </div>
-                    <p class="extracto">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-                    <a href="#" class="continuar">Continuar leyendo...</a>
-                </article>
-            </div>
 
+        <?php foreach($posts as $post) : ?>
             <div class="post">
                 <article>
-                    <h1 class="titulo"><a href="#">Titulo del artículo</a></h1>
+                    <h1 class="titulo"><a href="single.php?id=<?php echo $post['id']; ?>"><?php echo $post['titulo']; ?></a></h1>
                     <p class="fecha">Fecha del post</p>
                     <div class="thumb">
-                        <a href="#">
-                            <img src="<?php echo RUTA; ?>/img/1.jpg">
+                        <a href="single.php?id=<?php echo $post['id']; ?>">
+                            <img src="<?php echo RUTA; ?>/img/<?php echo $post['thumb']; ?>">
                         </a>
                     </div>
-                    <p class="extracto">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-                    <a href="#" class="continuar">Continuar leyendo...</a>
+                    <p class="extracto"><?php echo $post['extracto']; ?></p>
+                    <a href="single.php?id=<?php echo $post['id']; ?>" class="continuar">Continuar leyendo...</a>
                 </article>
             </div>
+        <?php endforeach; ?>
 
-            <div class="post">
-                <article>
-                    <h1 class="titulo"><a href="#">Titulo del artículo</a></h1>
-                    <p class="fecha">Fecha del post</p>
-                    <div class="thumb">
-                        <a href="#">
-                            <img src="<?php echo RUTA; ?>/img/1.jpg">
-                        </a>
-                    </div>
-                    <p class="extracto">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-                    <a href="#" class="continuar">Continuar leyendo...</a>
-                </article>
-            </div>
         </div>
+        
 
         <div class="side-bar">
         
