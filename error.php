ef8e83ee2b47c77606426b66941a83419e1f99ab
LuisChavezz <ing.luischavezz@gmail.com>
2021-04-12 00:23:31 -0500

    Error.php

---
+++ error.php
@@ -0,0 +1,16 @@
+<?php
+
+require 'admin/config.php';
+require 'views/header.php';
+
+?>
+
+<div class="container">
+    <div class="posts">
+        <div class="post">
+            <article>
+                <h1 class="titulo">ERROR 404 :(</h1>
+            </article>
+        </div>
+    </div>
+</div>
--- views/index.view.php
+++ views/index.view.php
@@ -7,7 +7,7 @@
             <div class="post">
                 <article>
                     <h1 class="titulo"><a href="single.php?id=<?php echo $post['id']; ?>"><?php echo $post['titulo']; ?></a></h1>
-                    <p class="fecha"><?php echo $post['fecha']; ?></p>
+                    <p class="fecha"><?php echo fecha($post['fecha']); ?></p>
                     <div class="thumb">
                         <a href="single.php?id=<?php echo $post['id']; ?>">
                             <img src="<?php echo RUTA; ?>/img/<?php echo $post['thumb']; ?>">
